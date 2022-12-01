6fd9d955b0f93cb0063158e7dc5d56663bf279bc
Muhammad Irfan Ali <muhammadirfanali@Muhammads-MacBook-Pro.local>
2022-12-01 13:51:44 +0500

    Develop the divider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Divider from './components/Divider';
 import Multiplier from './components/Multiplier';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <div className='max-w-7xl mx-auto px-4 py-10'>
         <div className='grid grid-cols-2 gap-8'>
           <Multiplier />
+          <Divider />
         </div>
       </div>
     </div>
--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,26 +1,28 @@
 import { useState } from 'react';
 
-const Multiplier = () => {
-  const [counter, setCounter] = useState(-5);
-  const [multiplierAns, setMultiplierAns] = useState(0);
+const Divider = () => {
+  const [counter, setCounter] = useState(6);
+  const [dividerAns, setDividerAns] = useState(0);
 
   return (
     <div className='overflow-hidden bg-white shadow sm:rounded-lg'>
       <div className='px-4 py-5 sm:px-6'>
         <h1 className='text-3xl font-bold text-center mb-5'>
-          Multiplier: {counter}
+          Divider: {counter}
         </h1>
         <input
           type='text'
           className='w-full border border-gray-300 px-5 py-3 rounded-md focus:outline-none'
           placeholder='Enter Value'
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setMultiplierAns(counter * +e.target.value)
+            +e.target.value
+              ? setDividerAns(counter / +e.target.value)
+              : setDividerAns(0)
           }
         />
         <div className='w-fit ml-auto mt-5 border border-gray-300 rounded-md p-5'>
           <p className='text-gray-700 text-xl'>
-            {multiplierAns ? multiplierAns : 'Enter the value'}
+            {dividerAns ? dividerAns : 'Enter the value'}
           </p>
         </div>
       </div>
@@ -28,4 +30,4 @@ const Multiplier = () => {
   );
 };
 
-export default Multiplier;
+export default Divider;
