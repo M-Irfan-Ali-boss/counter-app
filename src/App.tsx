5dfa7fe2f901d12db572c330cde1e13b93e727bb
Muhammad Irfan Ali <muhammadirfanali@Muhammads-MacBook-Pro.local>
2022-12-01 14:04:42 +0500

    Develop the counter and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Counter from './components/Counter';
 import Divider from './components/Divider';
 import Multiplier from './components/Multiplier';
 import Subtracter from './components/Subtracter';
@@ -5,11 +6,15 @@ import Subtracter from './components/Subtracter';
 function App() {
   return (
     <div className='App w-full'>
+      <div className='bg-gray-800 text-center p-10'>
+        <h1 className='text-white text-3xl italic font-bold'>Counter</h1>
+      </div>
       <div className='max-w-7xl mx-auto px-4 py-10'>
-        <div className='grid grid-cols-2 gap-8'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
           <Multiplier />
           <Subtracter />
           <Divider />
+          <Counter />
         </div>
       </div>
     </div>
---
+++ src/components/Counter.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+const Counter = () => {
+  const [counter, setCounter] = useState(7);
+
+  return (
+    <div className='overflow-hidden bg-white shadow sm:rounded-lg'>
+      <div className='px-4 py-5 sm:px-6'>
+        <h1 className='text-3xl font-bold text-center mb-5'>Counter</h1>
+        <div className='grid grid-cols-3 gap-4 align-items-center'>
+          <button
+            onClick={() => setCounter(counter + 1)}
+            className='bg-blue-500 text-white py-3 rounded-md text-center w-full'>
+            +
+          </button>
+          <h1 className='text-3xl font-bold text-center mb-5'>{counter}</h1>
+          <button
+            onClick={() => setCounter(counter - 1)}
+            className='bg-red-500 text-white rounded-md py-3 text-center w-full'>
+            -
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
