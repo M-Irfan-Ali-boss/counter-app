91cb2e87fd4ed8a1127e4719cf0cd419ab88c3d5
Muhammad Irfan Ali <muhammadirfanali@Muhammads-MacBook-Pro.local>
2022-12-01 13:46:41 +0500

    Develop the multipler

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Multiplier from './components/Multiplier';
+
 function App() {
   return (
-    <div className="App">
-      Hellow
+    <div className='App w-full'>
+      <div className='max-w-7xl mx-auto px-4 py-10'>
+        <div className='grid grid-cols-2 gap-8'>
+          <Multiplier />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Multiplier.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const Multiplier = () => {
+  const [counter, setCounter] = useState(-5);
+  const [multiplierAns, setMultiplierAns] = useState(0);
+
+  return (
+    <div className='overflow-hidden bg-white shadow sm:rounded-lg'>
+      <div className='px-4 py-5 sm:px-6'>
+        <h1 className='text-3xl font-bold text-center mb-5'>{counter}</h1>
+        <input
+          type='text'
+          className='w-full border border-gray-300 px-5 py-3 rounded-md focus:outline-none'
+          placeholder='Enter Value'
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setMultiplierAns(counter * +e.target.value)
+          }
+        />
+        <div className='w-fit ml-auto mt-5 border border-gray-300 rounded-md p-5'>
+          <p className='text-gray-700 text-xl'>
+            {multiplierAns ? multiplierAns : 'Enter the value'}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Multiplier;
